ed8ab0ea8e4f467de968da64a07498d763d94ce5
Antonio <don_ne@mail.ru>
2022-04-10 19:47:59 +0400

    Привет Маша

---
+++ Example005_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, эт же Машка");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
